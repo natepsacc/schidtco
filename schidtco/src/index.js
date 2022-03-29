3914d3a4ad57815f7f6dacbce27d3c7803c14b53
Nathan Schmidt <natepsacc@gmail.com>
2022-03-29 15:23:00 -0700

    mar29

--- schidtco/src/index.js
+++ schidtco/src/index.js
@@ -3,120 +3,26 @@ import ReactDOM from 'react-dom';
 import fleekStorage from '@fleekhq/fleek-storage-js';
 import './index.css';
 
-function Square(props) {
+class MainC extends React.Component {
+render(){
+const imgSrc = ('https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/smaller.jpg');
+
 	return (
-	  <button className="square" onClick={props.onClick}>
-	    {props.value}
-	  </button>
+<div id="inline" >
+		<div id="centerDiv">
+		 <img id="mainImage" alt="ipfsimage"  src={imgSrc}/>
+		</div>
+		<div id="mImageDesc">
+			<h1>this file is hosted on IPFS</h1>
+			<p> The link to the container is <a href={imgSrc}> {imgSrc} </a> </p>
+		</div>
+</div>
 	);
-}
-class Board extends React.Component {
-	constructor(props){
-	  super(props);
-	  this.state = {
-	     squares: Array(9).fill(null),
-	  xIsNext: true,
-	  };
 	}
-	
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		if (calculateWinner(squares) || squares[i]) {
-	return;
-	}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this .setState({
-		squares: squares,
-		xIsNext: !this.state.xIsNext,
-	});
-	}
-
-
-  renderSquare(i) {
-    return (
-	 <Square value={this.state.squares[i]}
-    	onClick={() => this.handleClick(i)}
- />
-);
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-	if (winner) {
-	  status = 'winner ' + winner;
-	} else {
-	  status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-return (
-     <img 
-      src="https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/smaller.jpg"
-      alt="hosted on ipfs"
-      />
-   );
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <MainC />,
   document.getElementById('root')
 );
-
 
