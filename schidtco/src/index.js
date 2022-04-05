10cbf52e2d1f71274805f7b7099cd9f8da9ab466
Nathan Schmidt <natepsacc@gmail.com>
2022-04-04 21:30:53 -0700

    accepting Defeat but workable site.

--- schidtco/src/index.js
+++ schidtco/src/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import fleekStorage from '@fleekhq/fleek-storage-js';
 import './index.css';
-
-class MainC extends React.Component {
-render(){
-const imgSrc = ('https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/smaller.jpg');
-const bgImgSrc = ("https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/a7exp-1-4.jpg");
-	return (
-<div id="everything" >
-	<img id="bgImg" alt="backgroundImg" src={bgImgSrc}/>
-		<div id="centerDiv">
-		 <img id="mainImage" alt="ipfsimage"  src={imgSrc}/>
-		</div>
-		<div id="mImageDesc">
-			<h1>this file is hosted on IPFS</h1>
-			<p> The link to the container is <a href={imgSrc}> {imgSrc} </a> </p>
-		</div>
-</div>
-	);
-	}
-}
+import useImageColor from 'use-image-color';
+import App from './app.js';
 
 ReactDOM.render(
-  <MainC />,
+  <App />,
   document.getElementById('root')
 );
 
