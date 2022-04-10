33767b63d4c5aeb5ab87354ae1a53b6c68e9a268
Nathan Schmidt <natepsacc@gmail.com>
2022-04-09 18:52:43 -0700

    forgot commas

--- schidtco/src/index.js
+++ schidtco/src/index.js
@@ -7,10 +7,8 @@ import App from './app.js';
 import WebCam from './reactwebcam.js';
 
 ReactDOM.render(
-  <App />
-  
-  document.getElementById('root')
-  document.getElementById('toor')
-
+<App />,
+document.getElementById('root'),
+document.getElementById('toor')
 );
 
