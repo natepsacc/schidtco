65282460c9b65d32a792684762f43993cfa29c29
Nathan Schmidt <natepsacc@gmail.com>
2022-05-12 23:49:35 -0700

    lastModofNightOne

--- schidtco/src/imageList.js
+++ schidtco/src/imageList.js
@@ -40,6 +40,7 @@ export default function StandardImageList() {
            src={`${item.img}`}
            srcSet={`${item.img}w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
            alt={item.title}
+           loading="lazy"
               />
        </ImageListItem>
      ))}
