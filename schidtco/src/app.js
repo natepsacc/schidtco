1415e781b76eb3e75fcc9823fe57ae76a9eff1a3
Nathan Schmidt <natepsacc@gmail.com>
2022-06-14 11:33:14 -0700

    new logo Dynamics

--- schidtco/src/app.js
+++ schidtco/src/app.js
@@ -184,7 +184,7 @@ function Logo(){
 	return(
 		<span>
 		<a href="/" >
-	<img  id="TitleLogo" alt="Schidt.co Logo" src="https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/logos/fuck/schidtcoLogo.png"/>
+	<img  id="TitleLogo" alt="Schidt.co Logo" src="https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/pictures/logos/IMG_7045overlayBlue.svg"/>
 		</a>
 
 		<div id="nav" class="nav">
