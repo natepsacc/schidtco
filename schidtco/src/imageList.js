5cfda159c4613aaeeba1dc92efa8706ebfe292af
natepsacc <natepsacc@gmail.com>
2022-06-16 07:46:14 -0700

    id?

--- schidtco/src/imageList.js
+++ schidtco/src/imageList.js
@@ -72,7 +72,7 @@ console.log(gridNum);
      {sort.map((item) => (
        <ImageListItem key={item.img} id="height" sx={{m:0}}>
         <LazyLoadImage
-        id="imgg"
+        id="im"
               effect="blur"
              alt={item.title}
              src={item.img} // use normal <img> attributes as props
