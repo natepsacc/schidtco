e62b2a0105eb43af063329f8063a4cd00a12c37d
Nathan Schmidt <natepsacc@gmail.com>
2022-06-24 02:11:36 -0700

    corect

--- schidtco/src/imageList.js
+++ schidtco/src/imageList.js
@@ -76,7 +76,7 @@ const imageClick = (props) => {
        display: "grid",
        gridTemplateColumns:{
          mobile: clicked ? 'repeat(2, 50%)' : 'repeat(1, 100%)',
-         bigMobile: clicked ? 'repeat(1, 100%)' : 'repeat(2, 50%)',
+         bigMobile: clicked ? 'repeat(2, 50%)' : 'repeat(1, 100%)',
          desktop: clicked ? 'repeat(1, 100%)' : 'repeat(2, 50%)',
        },
        [`& .${imageListItemClasses.root}`]: {
