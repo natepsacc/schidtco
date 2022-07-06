50e39a448522c850ecb913406c32b7cf99082060
System Administrator <root@503800462.lan>
2022-07-05 22:59:25 -0700

    remove

--- schidtco/src/imageList.js
+++ schidtco/src/imageList.js
@@ -751,14 +751,6 @@ const itemData = [
 
         mSrc: 'javascript:;',
     }, {
-        img: 'https://i.imgur.com/KOLnn80.jpg',
-        date: 'STAR THEME',
-        title: '062022',
-        tag: 'RAD',
-        tag2: '',
-
-        mSrc: 'javascript:;',
-    }, {
         img: 'https://i.imgur.com/dC2cqoa.jpg',
         title: 'R%D',
         date: '071522',
