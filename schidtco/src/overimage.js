dbfb56661475f2b3bd89b0c2bdadc6edf135fd5a
ihateyou <natepsacc@gmail.com>
2022-08-20 18:53:05 -0400

    cv square

--- schidtco/src/overimage.js
+++ schidtco/src/overimage.js
@@ -6,7 +6,7 @@ const TimeImage = () => {
      document.getElementById('overimage').style.left= getRandom(0, 1720)+'px'; // 👈🏼 Horizontally
      document.getElementById('overimage').style.top = getRandom(0, 1080)+'px'; // 👈🏼 Vertically
 
-  }, 500);
+  }, 670);
 return(
   <>
   <img id='overimage' src='https://i.imgur.com/Q0InM8M.png'  alt=""/>
